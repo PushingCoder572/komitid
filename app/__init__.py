2f9375cf19558dca0da92de2c940152cb262e45f
Olle Jernström <olle.jernstrom@gmail.com>
2020-08-27 18:35:48 +0200

    skills

---
+++ app/__init__.py
@@ -0,0 +1,6 @@
+"""
+Created on 2020-08-27
+
+Project: komitid
+@author: ollejernstrom
+"""
---
+++ app/models.py
@@ -0,0 +1,34 @@
+"""
+Created on 2020-08-27
+
+Purpose: Interacts with the database
+Project: komitid
+@author: ollejernstrom
+"""
+
+"""
+Created on 2020-08-25
+
+Project: social
+@author: ollejernstrom
+"""
+import sqlite3 as sql
+from os import path
+
+ROOT = path.dirname(path.realpath(__file__))
+
+
+def create_user(uname, pword):
+    connection = sql.connect(path.join(ROOT, 'database.db'))
+    cursor = connection.cursor()
+    cursor.execute('insert into users (name, content) values(?, ?)', (uname, pword))
+    connection.commit()
+    connection.close()
+
+
+def get_posts():
+    connection = sql.connect(path.join(ROOT, 'database.db'))
+    cursor = connection.cursor()
+    cursor.execute('select * from posts')
+    posts = cursor.fetchall()
+    return posts
