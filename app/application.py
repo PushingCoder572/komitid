5d5303e69a8727703100fdc0768dee8455626e64
Olle Jernström <olle.jernstrom@gmail.com>
2020-09-03 23:31:11 +0200

    Fullständig login med session user

--- app/application.py
+++ app/application.py
@@ -6,36 +6,69 @@ Project: komitid
 """
 
 # Libraries and frameworks
-from flask import Flask, render_template, redirect, request
-from app.models import checkuser
+from flask import Flask, g, render_template, redirect, request, url_for, session
 
+from app.models import checkuser,get_data, get_session_user, db_query, create_user
 
 # Setup
 app = Flask(__name__)
+app.secret_key = 'ennyckeljävelbarajagkan'
 
 
-@app.route('/signup')
+@app.before_request
+def before_request():
+    if 'user_id' in session:
+        user = get_session_user(session['user_id'])
+        g.user = user
+
+
+@app.route('/home')
+def home():
+    print(g.user.id)
+    return render_template('sites/home.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        re_password = request.form.get('re_password')
+        existing_usernames = db_query('name')
+
+        if username not in existing_usernames and password == re_password:
+            create_user(username, password)
+            print('User Created')
+            return redirect(url_for('login'))
+        print('Could not create account')
+
     return render_template('sites/signup.html')
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
+        session.pop('user_id', None)  # Removes existing user
+
+        # Gets form data
         uname = request.form.get('username')
         pword = request.form.get('password')
-        if uname and pword:
-            if checkuser(uname, pword):
-                return redirect('/home')
-        return render_template('login.html')
+
+        # Checks if user exists
+        userid = checkuser(uname, pword)
+        if userid:
+            session['user_id'] = userid
+            return redirect(url_for('home'))
+
+        # flash an Error msg
+        return redirect(url_for('login'))
+
     return render_template('sites/login.html')
 
 
 @app.route('/')
 def index():
     return redirect('/login')
-
-
 
 
 if __name__ == '__main__':
--- app/models.py
+++ app/models.py
@@ -6,17 +6,29 @@ Project: komitid
 @author: ollejernstrom
 """
 
-"""
-Created on 2020-08-25
-
-Project: social
-@author: ollejernstrom
-"""
 import sqlite3 as sql
 from os import path
-
+from flask_login import UserMixin
 
 ROOT = path.dirname(path.realpath(__file__))
+
+
+class User(UserMixin):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.password = password
+        self.username = username
+
+
+"""
+    def __repr__(self):
+        return f'User#{self.id} {self.username}'"""
+
+
+def get_session_user(user_id):
+    userdata = get_user_by_id(user_id)
+    userobj = User(*userdata)
+    return userobj
 
 
 def create_user(uname, pword):
@@ -26,13 +38,19 @@ def create_user(uname, pword):
     connection.commit()
     connection.close()
 
+def db_query(key):
+    connection = sql.connect(path.join(ROOT, 'database.db'))
+    cursor = connection.cursor()
+    cursor.execute(f'SELECT {key} from users')
+    data = cursor.fetchall()
+    return data
 
-def get_posts():
+def get_data():
     connection = sql.connect(path.join(ROOT, 'database.db'))
     cursor = connection.cursor()
     cursor.execute('select * from users')
-    posts = cursor.fetchall()
-    return posts
+    data = cursor.fetchall()
+    return data
 
 
 def delete_user(uname):
@@ -43,13 +61,19 @@ def delete_user(uname):
     connection.close()
 
 
+def get_user_by_id(user_id):
+    db_users = get_data()
+    if db_users:
+        for user in db_users:
+            if user_id == user[0]:
+                return user
+    return False
+
+
 def checkuser(try_uname, try_pword):
-    valid_users = get_posts()
+    valid_users = get_data()
     if valid_users:
-        for db_id, uname, pword in valid_users:
-            if try_uname == uname and try_pword == pword:
-                print(True)
-                return True
-
-        return False
-
+        for user in valid_users:
+            if try_uname == user[1] and try_pword == user[2]:
+                return user[0]
+    return False
