5bdc486e8a16641034ad8aa7e9aaa5e9b7aefe1d
asper <asperk12@gmail.com>
2020-10-21 21:28:27 +0200

    Hemsida Heroku Patch

---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""
+Created on 2020-08-27
+
+Project: komitid
+@author: ollejernstrom
+"""
+
+import app
+
+app.static_folder = 'resources/static'
--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Created on 2020-08-27
-
-Project: komitid
-@author: ollejernstrom
-"""
-from app import application
-
-application.static_folder = 'static'
